b1035d10e6b43578e1dabb6c69d8a83392587806
andrey-justo-movile <andrey.justo@movile.com>
2017-03-26 18:11:05 -0300

    refactor searching

--- surf.py
+++ surf.py
@@ -4,28 +4,29 @@ Created on Mar 25, 2017
 @author: andrey.justo
 '''
 
-import numpy as np
-
+import time
 from os import path
 from os import listdir
 from os.path import isfile, join
 import pickle as pkl
 
-#opencv library
+# sklearn libs
+from sklearn.cross_validation import train_test_split
+import sklearn.preprocessing as preprocessing
+import numpy as np
+
+#opencv libs
 import cv2
 from cv2 import xfeatures2d
 
 # lopq
-from sklearn.cross_validation import train_test_split
-
 from lopq import LOPQModel, LOPQSearcher
 from lopq.eval import compute_all_neighbors, get_recall
 from lopq.model import eigenvalue_allocation
-from lopq import utils
 
 relpath = lambda x: path.abspath(path.join(path.dirname(__file__), x))
 
-def load_feature_vectors(image_path, fvector_path, should_store):
+def load_feature_vectors(image_path, fvector_path, hessian_threshold, should_store):
     # Load the images
     img = cv2.imread(image_path)
     if img is None:
@@ -34,9 +35,11 @@ def load_feature_vectors(image_path, fvector_path, should_store):
     g_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
  
     # SURF extraction
-    surf = xfeatures2d.SURF_create()
+    surf = xfeatures2d.SURF_create(hessian_threshold)
+    surf.setExtended(True)
     kp, desc = surf.detectAndCompute(g_img, None)
- 
+    print('For:', image_path, 'KeyPoints:', len(kp), 'Descriptor Shape:', desc.shape if desc is not None else None)
+    
     if (should_store):
         write_features(kp, desc, fvector_path)
  
@@ -47,7 +50,7 @@ def write_features(kp, desc, file_path):
     f.write(pkl.dumps(np.array(desc)))
     f.close()
 
-def load_feature_vectors_from_sample(images_trainning_dir, feature_vector_dir, should_store):
+def load_feature_vectors_from_sample(images_trainning_dir, feature_vector_dir, hessian_threshold, should_store):
     
     all_features = None
     for f in listdir(images_trainning_dir):
@@ -55,15 +58,19 @@ def load_feature_vectors_from_sample(images_trainning_dir, feature_vector_dir, s
         file_name = path.basename(file_path).split('.')
 
         if isfile(file_path) and len(file_name) > 1:
-            features = load_feature_vectors(file_path, feature_vector_dir + file_name[0], should_store)
+            features = load_feature_vectors(file_path, feature_vector_dir + file_name[0], hessian_threshold, should_store)
             if features[1] is not None:
+                norm = normalize_feature(features[1])
                 if all_features is None:
-                    all_features = features[1]
+                    all_features = norm
                 else:
-                    all_features = np.vstack((all_features, features[1]))
+                    all_features = np.vstack((all_features, norm))
             
     return all_features
-        
+
+def normalize_feature(data):
+    return preprocessing.normalize(data, norm='l2')
+
 def pca(data):
     """
     A simple PCA implementation that demonstrates how eigenvalue allocation
@@ -94,20 +101,19 @@ def pca(data):
     return P, mu
 
 def main():
-    """
-    A brief demo script showing how to train various LOPQ models with brief
-    discussion of trade offs.
-    """
     
     images_trainning_dir = './data/sample/'
     feature_vector_dir = './data/vectors/'
-
+    hessian_threshold = 5000
+    
     # Loading dataset
-    data = load_feature_vectors_from_sample(images_trainning_dir, feature_vector_dir, False)
+    a = time.time()
+    data = load_feature_vectors_from_sample(images_trainning_dir, feature_vector_dir, hessian_threshold, False)
+    print('Finished Loading SURF vectors:', (time.time() - a))
 
     # about this dataset.
     P, mu = pca(data)
-
+ 
     # Mean center and rotate the data; includes dimension permutation.
     # It is worthwhile see how this affects recall performance. On this
     # dataset, which is already PCA'd from higher dimensional features,
@@ -116,63 +122,59 @@ def main():
     # on the dataset.
     data = data - mu
     data = np.dot(data, P)
+    print('After PCA')
 
     # Create a train and test split. The test split will become
     # a set of queries for which we will compute the true nearest neighbors.
-    train, test = train_test_split(data, test_size=0.2)
-
+    random_state = 40
+    train, test = train_test_split(data, test_size=0.2, random_state=random_state)
+    print('After Split data')
+    
     # Compute distance-sorted neighbors in training set for each point in test set.
     # These will be our groundtruth for recall evaluation.
+    a = time.time()
     nns = compute_all_neighbors(test, train)
+    print('Finished Calculating NNs:', (time.time() - a))
+    
+    # First test parameters
+    a = time.time()
+    m = create_model(train, 16, 8, 256, None)
+    print('Finish Fiting Model:', (time.time() - a))
 
-    # Fit model
-    m = LOPQModel(V=16, M=8)
-    m.fit(train, n_init=1)
+    a = time.time()
+    search_and_recall(m, train, test, nns)
+    print('Finish Searching in Model:', (time.time() - a))
 
-    # Note that we didn't specify a random seed for fitting the model, so different
-    # runs will be different. You may also see a warning that some local projections
-    # can't be estimated because too few points fall in a cluster. This is ok for the
-    # purposes of this demo, but you might want to avoid this by increasing the amount
-    # of training data or decreasing the number of clusters (the V hyperparameter).
+    # Second test parameters
+    a = time.time()
+    m2 = create_model(train, 16, 16, 256, (m.Cs, None, None, None))
+    print('Finish Fiting Model:', (time.time() - a))
 
-    # With a model in hand, we can test it's recall. We populate a LOPQSearcher
-    # instance with data and get recall stats. By default, we will retrieve 1000
-    # ranked results for each query vector for recall evaluation.
-    searcher = LOPQSearcher(m)
+    a = time.time()
+    search_and_recall(m2, train, test, nns)
+    print('Finish Searching Model:', (time.time() - a))
+
+    # Third test parameters
+    a = time.time()
+    m3 = create_model(train, 16, 8, 512, (m.Cs, m.Rs, m.mus, None))
+    print('Finish Fiting Model:', (time.time() - a))
+
+    a = time.time()
+    search_and_recall(m3, train, test, nns)
+    print('Finish Searching Model:', (time.time() - a))
+
+
+def create_model(train, v, m, clusters, parameters):
+    model = LOPQModel(V=v, M=m, subquantizer_clusters=clusters, parameters=parameters)
+    model.fit(train, n_init=1)
+    return model
+
+def search_and_recall(model, train, test, nns):
+    searcher = LOPQSearcher(model)
     searcher.add_data(train)
     recall, _ = get_recall(searcher, test, nns)
-    print 'Recall (V=%d, M=%d, subquants=%d): %s' % (m.V, m.M, m.subquantizer_clusters, str(recall))
-
-    # We can experiment with other hyperparameters without discarding all
-    # parameters everytime. Here we train a new model that uses the same coarse
-    # quantizers but a higher number of subquantizers, i.e. we increase M.
-    m2 = LOPQModel(V=16, M=16, parameters=(m.Cs, None, None, None))
-    m2.fit(train, n_init=1)
-
-    # Let's evaluate again.
-    searcher = LOPQSearcher(m2)
-    searcher.add_data(train)
-    recall, _ = get_recall(searcher, test, nns)
-    print 'Recall (V=%d, M=%d, subquants=%d): %s' % (m2.V, m2.M, m2.subquantizer_clusters, str(recall))
-
-    # The recall is probably higher. We got better recall with a finer quantization
-    # at the expense of more data required for index items.
-
-    # We can also hold both coarse quantizers and rotations fixed and see what
-    # increasing the number of subquantizer clusters does to performance.
-    m3 = LOPQModel(V=16, M=8, subquantizer_clusters=512, parameters=(m.Cs, m.Rs, m.mus, None))
-    m3.fit(train, n_init=1)
-
-    searcher = LOPQSearcher(m3)
-    searcher.add_data(train)
-    recall, _ = get_recall(searcher, test, nns)
-    print 'Recall (V=%d, M=%d, subquants=%d): %s' % (m3.V, m3.M, m3.subquantizer_clusters, str(recall))
-
-    # The recall is probably better than the first but worse than the second. We increased recall
-    # only a little by increasing the number of model parameters (double the subquatizer centroids),
-    # the index storage requirement (another bit for each fine code), and distance computation time
-    # (double the subquantizer centroids).
-
+    print 'Recall (V=%d, M=%d, subquants=%d): %s' % (model.V, model.M, model.subquantizer_clusters, str(recall))
+    
 
 if __name__ == '__main__':
     main()
